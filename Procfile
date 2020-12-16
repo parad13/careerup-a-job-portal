f203c4d2f024d5d3455760d046b2e4c58cdcac22
paras waral <56946866+parad13@users.noreply.github.com>
2020-12-16 19:14:22 +0530

    'initial'

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
-release: ./release-tasks.sh
+release: 
+    python manage.py migrate 
+    pip freeze > requirements.txt
 web: gunicorn CareerRide.wsgi --log-file -
--- env.py
+++ env.py
@@ -6,3 +6,5 @@ pass1 = os.environ.get('DATABASE_PASS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 print(SECRET_KEY,DEBUG,pass1,EMAIL_HOST_PASSWORD)
+
+#release: ./release-tasks.sh
