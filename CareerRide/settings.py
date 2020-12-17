671b0681113c247c06c3ce85db98643393e87c97
paras waral <56946866+parad13@users.noreply.github.com>
2020-12-17 17:38:50 +0530

    'intial'

--- CareerRide/settings.py
+++ CareerRide/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # import django_dotenv
 
 # import environ
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
