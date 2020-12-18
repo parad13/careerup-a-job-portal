acd1068678c23a4046ce006e9cc326eeafacb401
paras waral <56946866+parad13@users.noreply.github.com>
2020-12-18 08:21:51 +0530

    'intial'

--- CareerRide/settings.py
+++ CareerRide/settings.py
@@ -1,34 +1,15 @@
 import os
 import django_heroku
-from pathlib import Path
-# import django_dotenv
+import socket
 
-# import environ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'gqam)^m3zh!ej#v4xj!k(8pk1y7p__xv_iwu32$@s2-*^u!wz6'
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'gqam)^m3zh!ej#v4xj!k(8pk1y7p__xv_iwu32$@s2-*^u!wz6'
-# SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = (os.environ.get('DEBUG_VALUE') == True)
-# DEBUG = True #for development
+DEBUG = True  # for development
 # DEBUG = False # for production
 
-ALLOWED_HOSTS = ['https://guarded-reaches-21647.herokuapp.com']
-# ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -46,7 +27,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware', #added manually
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # added manually
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +42,7 @@ ROOT_URLCONF = 'CareerRide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,32 +61,12 @@ WSGI_APPLICATION = 'CareerRide.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# pass1 = os.environ.get('DATABASE_PASS')
 DBPASS = os.environ.get('DATABASE_PASS')
 DATABASES = {
-    # #MySQL setup
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'careerup',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'USER':'root',
-    #     'PASSWORD': '1234'
-    # },
-    #Postgres setup
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'careerup',
-        'USER': 'postgres',
-        'PASSWORD': DBPASS,
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     },
-    # #sqlite3 setup
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # },
 }
 
 
@@ -147,7 +108,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # staticfiles
+# staticfiles for deployment purpose only
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -170,13 +132,17 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-django_heroku.settings(locals()) 
+django_heroku.settings(locals())
 
-#For extra security layer while deployment
-SECURE_HSTS_SECONDS = 60
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_PRELOAD = True
-
+if os.environ.get('DJANGO_DEV') is not None:
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+    SECURE_HSTS_PRELOAD = False
+else:
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_PRELOAD = True
--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 release: python manage.py migrate 
-pip freeze > requirements.txt
+         pip freeze > requirements.txt
+
 web: gunicorn CareerRide.wsgi --log-file -
